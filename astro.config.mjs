4d363a0e89c7dcdad37cbfbbfa7d55d907f4135b
Joakim Tveter <joakim@tveter.net>
2024-02-28 20:45:25 +0100

    Added i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte({ preprocess: [] })]
+  integrations: [svelte({ preprocess: [] })],
+  i18n: {
+    defaultLocale: 'no',
+    locales: ['en', 'no'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
