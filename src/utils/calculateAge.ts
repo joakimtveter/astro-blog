ab61dcda335a0fe36cc8939d666131b8d489636f
Joakim Tveter <joakim@tveter.net>
2024-05-28 22:33:24 +0200

    Updated front page in norwegian

---
+++ src/utils/calculateAge.ts
@@ -0,0 +1,10 @@
+export default function calculateAge(birthday: string): number {
+  const today = new Date();
+  const birthDate = new Date(birthday);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const month = today.getMonth() - birthDate.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    age -= 1;
+  }
+  return age;
+}
